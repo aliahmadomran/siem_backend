2bb4b1ea99c5233c92a5f7d0c02e14fb665c7f5d
ali.5 <arabicnlp123@gmail.com>
2022-07-16 14:13:45 +0300

    query builder

--- elastic/queryBuilder.py
+++ elastic/queryBuilder.py
@@ -1,23 +1,29 @@
-
 import datetime
-
-from soupsieve import select
-
 class ElasticQuery():
     def __init__(self):
         self.query = {}
         pass
     
     def dateFilter(self,startDate,endDate):
+        
         try:
             start = startDate
-            start = datetime.datetime.strptime(start, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%dT%H:%M:%S")
+            if start=='':
+                start = datetime.datetime.now() - datetime.timedelta(days=365)
+                start.strftime("%Y-%m-%dT%H:%M:%SZ")
+            
+            else:
+                start = start.strftime("%Y-%m-%dT%H:%M:%SZ")
         except:
             pass
         try:
             end = endDate
-            end = datetime.datetime.strptime(end, "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%dT%H:%M:%S")
-
+            if end=='':
+                end = datetime.datetime.now()
+                end.strftime("%Y-%m-%dT%H:%M:%SZ")
+            
+            else:
+                end = end.strftime("%Y-%m-%dT%H:%M:%SZ")
         except:
             pass
         
@@ -32,8 +38,9 @@ class ElasticQuery():
             self.query['bool']["filter"] = {
                 "range": {
                     "date": {
-                        "gte": startDate,
-                        "lt": endDate
+                        "gte": str(start),
+                        "lt": str(end),
+                        "format":"yyyy-MM-dd'T'HH:mm:ssZ"
                     }
                 }
             }
@@ -42,11 +49,50 @@ class ElasticQuery():
         for term in terms:
             self.query["bool"]['must'].append({"term": {str(term['field'])+".keyword": term['value']}})
 
-
-    def Query(self,requestJson):
+    def queryResponce(self,field,size):
+        self.responce = {
+            "size":0,
+            "query":self.query,
+            "aggs":{
+                "Count":{
+                    "terms": {
+                    "field": str(field)+".keyword",
+                    "size": size
+                }
+                }
+            }
+        }
+        pass
+    def queryBuilder(self,requestJson={}):
         self.requestJson = requestJson
         self.dateFilter(self.requestJson['date_range']['start'],self.requestJson['date_range']['end'])
         self.addTerms(self.requestJson['conditions'])
-        #
+        self.queryResponce(field=self.requestJson['field'],size=self.requestJson['size'])
+        print('------------------------------------')
+        print(self.query)
+        print('------------------------------------')
+        print(self.responce)
+        print('------------------------------------')
+        
 
+obj = ElasticQuery()
 
+requestJ = {
+
+        "field":"agents",
+        "date_range":{
+            "start":"",
+            "end":""
+                        },
+        "conditions":[
+            {"field":"d",
+             "value":"e"},
+            {"field":"f",
+             "value":"g"},
+            {"field":"h",
+             "value":"i"}
+                    ],
+        "size":100
+}
+
+obj.queryBuilder(requestJson=requestJ)
